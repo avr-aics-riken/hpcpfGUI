a995d8281fe50d67069549eea4af2691d738d76e
kioku <gkioku@gmail.com>
2014-05-22 18:03:35 +0900

    beautify

--- app/ptl_server.js
+++ app/ptl_server.js
@@ -1,97 +1,99 @@
-var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
-var fs = require('fs');
-var http = require('http');
-
-var util = require('./util');
-
-var editorevent = require('./editor_event');
-var remotehostevent = require('./remotehost_event');
-
-var filedialog= require('./js/filedialog');
-var RemoteFTP = require('./js/RemoteFTP');
+var exec = require('child_process').exec,
+	spawn = require('child_process').spawn,
+	fs = require('fs'),
+	http = require('http'),
+	util = require('./util'),
+	editorevent = require('./editor_event'),
+	remotehostevent = require('./remotehost_event'),
+	filedialog = require('./js/filedialog'),
+	RemoteFTP = require('./js/RemoteFTP');
 
 //-------------------------------------
 // Server request logic
 //-------------------------------------
 var server = http.createServer();
-server.on('request', function(req,res){
-	console.log("REQ>"+req.url);
+server.on('request', function (req, res) {
+	"use strict";
+	console.log("REQ>" + req.url);
 	
-	var url = req.url;
-	var addrs = req.url.split("?");
-	if (addrs)
+	var url = req.url,
+		addrs = req.url.split("?"),
+		html,
+		ext;
+	if (addrs) {
 		url = addrs[0]; // ignore args
-
-	if (url == '/') {
-		var html = fs.readFileSync('./home.html');
-		res.writeHead(200,{'Content-Type':'text/html', charaset:'UTF-8'});
+	}
+	
+	if (url === '/') {
+		html = fs.readFileSync('./home.html');
+		res.writeHead(200, {'Content-Type': 'text/html', charaset: 'UTF-8'});
 		res.end(html);
 	} else {
-		fs.readFile('.' + url, function (err,data){
+		fs.readFile('.' + url, function (err, data) {
 			if (err) {
-				res.writeHead(404,{'Content-Type':'text/html', charaset:'UTF-8'});
+				res.writeHead(404, {'Content-Type': 'text/html', charaset: 'UTF-8'});
 				res.end("<h1>not found<h1>");
 				return;
 			}
-			var ext = util.getExtention(url);
-			if (ext == "css")
-				res.writeHead(200,{'Content-Type':'text/css',charaset:'UTF-8'});
-			else if (ext == "html" || ext == "htm")
-				res.writeHead(200,{'Content-Type':'text/html',charaset:'UTF-8'});
-			else if (ext == "js" || ext == "json")
-				res.writeHead(200,{'Content-Type':'text/javascript',charaset:'UTF-8'});
-			else
+			ext = util.getExtention(url);
+			if (ext === "css") {
+				res.writeHead(200, {'Content-Type': 'text/css', charaset: 'UTF-8'});
+			} else if (ext === "html" || ext === "htm") {
+				res.writeHead(200, {'Content-Type': 'text/html', charaset: 'UTF-8'});
+			} else if (ext === "js" || ext === "json") {
+				res.writeHead(200, {'Content-Type': 'text/javascript', charaset: 'UTF-8'});
+			} else {
 				res.writeHead(200);//,{charaset:'UTF-8'});
+			}
 			res.end(data);
 		}); // fs.readFile
 	}
 });
 server.listen(8080);
-console.log('Server running on localhost:8080')
+console.log('Server running on localhost:8080');
 
 //---------------------------------------------------------------------------
 
-function registerPTLEvent(socket)
-{
-	socket.on('ptl_launchapp', function(data) {
+function registerPTLEvent(socket) {
+	"use strict";
+	socket.on('ptl_launchapp', function (data) {
 		console.log("ptl_launchapp:" + data.appname);
 		
-		var appcmd;
-		if (data.appname == 'FXgen')
+		var appcmd, child;
+		if (data.appname === 'FXgen') {
 			appcmd = 'open /Applications/FXgen.app';
-		else if(data.appname == 'PDI')
+		} else if (data.appname === 'PDI') {
 			appcmd = 'pdi';
-		
-		var child = exec(appcmd, function(err, stdout, stderr) {
+		}
+		child = exec(appcmd, function (err, stdout, stderr) {
 			if (err) {
 				console.log(err);
 				console.log(err.code);
 				console.log(err.signal);
 			} else {
 				console.log('stdout: ' + stdout);
-				console.log('stderr: ' + stderr)
+				console.log('stderr: ' + stderr);
 			}
 		});
 	});// ptl_launchapp
 	
-	filedialog.SocketEvent(socket,'homedlg');
-	filedialog.SocketEvent(socket,'remotedlg');
+	filedialog.SocketEvent(socket, 'homedlg');
+	filedialog.SocketEvent(socket, 'remotedlg');
 
-	socket.on('registerProjectHistory', function(path){
-		console.log("REGISTER_HISTORY:"+path);
+	socket.on('registerProjectHistory', function (path) {
+		console.log("REGISTER_HISTORY:" + path);
 		var historyFile = "project_history.json";
-		fs.readFile(historyFile, function(err,data){
-			var names = path.split("/");
-			var name = names[names.length-1];
-			console.log("ProjName="+name);
+		fs.readFile(historyFile, function (err, data) {
+			var names = path.split("/"),
+				name = names[names.length - 1];
+			console.log("ProjName=" + name);
 			if (err) {
 				console.log('Error: ' + err);
-				data = new Array();
-				data.push({"name":name, "path":path});
+				data = [];
+				data.push({"name": name, "path": path});
 			} else {
 				data = JSON.parse(data);
-				data.splice(0, 0,{"name":name, "path":path}); // Add first
+				data.splice(0, 0, {"name": name, "path": path}); // Add first
 			}
 			data  = JSON.stringify(data);
 			fs.writeFileSync(historyFile, data, 'utf8');
@@ -106,8 +108,9 @@ var io = require('socket.io').listen(server);
 io.set('log level', 1);
 
 io.sockets.on('connection', function (socket) {
+	"use strict";
 	socket.emit('event', 'Server Connected.');
-	console.log("[CONNECT] ID="+socket.id);
+	console.log("[CONNECT] ID=" + socket.id);
 
 	RemoteFTP(socket);
 	editorevent.registerEditorEvent(socket);
