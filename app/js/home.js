254bed0437cee96d460d03abd9013a9be617b191
kioku <gkioku@gmail.com>
2014-05-22 16:01:37 +0900

    beautify.

--- app/js/home.js
+++ app/js/home.js
@@ -1,9 +1,9 @@
-"use strict";
-var socket;
-socket = io.connect();
-var filedialog = new FileDialog('homedlg',true,true);
+var console, io, FileDialog,
+	socket = io.connect(),
+	filedialog = new FileDialog('homedlg', true, true);
 
-socket.on('connect', function() { // 2
+socket.on('connect', function () { // 2
+	"use strict";
 	console.log('connected');
 	socket.on('event', function (data) {
 		console.log(data);
@@ -11,25 +11,48 @@ socket.on('connect', function() { // 2
 	filedialog.registerSocketEvent(socket);
 });
 
-function bootstrap()
-{
-	updateProjectList();
-}
+//-------------
+// project history
 
-function updateProjectList()
-{
+var xrequet = function (url, callback_online, callback_offline) {
+	"use strict";
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "project_history.json", true);
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4 && xhr.status === 200) { // online
+			if (callback_online) {
+				callback_online(this.responseText);
+			}
+		}
+		if (xhr.readyState === 4 && xhr.status === 0) { // offline
+			if (callback_offline) {
+				callback_offline(this.responseText);
+			}
+		}
+	};
+	xhr.send(null);
+};
+
+function updateProjectList() {
+	"use strict";
 	function readProjectHistory(data) {
-		var proj = JSON.parse(data);
-		for (var i = 0; i<proj.length; ++i){
-			var plist = document.getElementById("projectHistoryList");
-			var newitem = document.getElementById("projectHistoryList_itemtemplate").cloneNode(true);
+		var proj = JSON.parse(data),
+			plist,
+			newitem,
+			projectname,
+			projectpath,
+			label,
+			i;
+		for (i = 0; i < proj.length; i += 1) {
+			plist = document.getElementById("projectHistoryList");
+			newitem = document.getElementById("projectHistoryList_itemtemplate").cloneNode(true);
 			newitem.id = "";
 		
 			// setting new item
-			var projectname = proj[i].name;//"PROJECT:01";
-			var projectpath = proj[i].path;//"/Users/username/project01/";
+			projectname = proj[i].name;//"PROJECT:01";
+			projectpath = proj[i].path;//"/Users/username/project01/";
 		
-			var label = newitem.firstChild.nextSibling;
+			label = newitem.firstChild.nextSibling;
 			label.innerHTML = projectname;
 			newitem.path = projectpath;
 			plist.appendChild(newitem);
@@ -38,96 +61,84 @@ function updateProjectList()
 	xrequet("project_history.json", readProjectHistory, readProjectHistory);
 }
 
-function openProject(path)
-{
+function bootstrap() {
+	"use strict";
+	updateProjectList();
+}
+
+function openProject(path) {
+	"use strict";
 	updateProjectList();
 	console.log(path);
-	window.open("editor.html?"+path,"_blank");// KVTools
+	window.open("editor.html?" + path, "_blank");// KVTools
 }
 
-function launchApp_FXgen()
-{
-	socket.emit('ptl_launchapp',{appname:'FXgen'});
+function launchApp_FXgen() {
+	"use strict";
+	socket.emit('ptl_launchapp', {appname : 'FXgen'});
 }
-function launchApp_PDI()
-{
-	socket.emit('ptl_launchapp',{appname:'PDI'});
+function launchApp_PDI() {
+	"use strict";
+	socket.emit('ptl_launchapp', {appname : 'PDI'});
 }
-function launchApp_KVTools()
-{
-	window.open(location.hostname+":8082","_blank");// KVTools
+function launchApp_KVTools() {
+	"use strict";
+	window.open(location.hostname + ":8082", "_blank");// KVTools
 }
 
-function newProject()
-{
+function newProject() {
+	"use strict";
 	// TODO
 }
 
-function registerRemoteHost()
-{
-	var s = window.open("remotehost.html","remotehost");
+function registerRemoteHost() {
+	"use strict";
+	var s = window.open("remotehost.html", "remotehost");
 	s.focus(); // TODO: for firefox
 }
 
-function openFileBrowser()
-{
-	var s = window.open("filebrowser.html","filebrowser");
+function openFileBrowser() {
+	"use strict";
+	var s = window.open("filebrowser.html", "filebrowser");
 	s.focus(); // TODO: for firefox
 }
 
-function openKDB()
-{
-	window.open("http://www.cenav.org/kdb/","_blank");
+function openKDB() {
+	"use strict";
+	window.open("http://www.cenav.org/kdb/", "_blank");
 }
 
 //----------------------------------
 
-function registerProjectHistory(path)
-{
-	socket.emit("registerProjectHistory",path);
+function registerProjectHistory(path) {
+	"use strict";
+	socket.emit("registerProjectHistory", path);
 }
 
 //-----------------------------------
-function openfileDialog(path)
-{
+var tarPath;
+function openfileDialog(path) {
+	"use strict";
 	tarPath = path;
-	var i = document.getElementsByClassName("popup_center")[0];
-	i.style.display="block";
+	var i, c;
+	i = document.getElementsByClassName("popup_center")[0];
+	i.style.display = "block";
 
-	var c = document.getElementById('projdir_path');
+	c = document.getElementById('projdir_path');
 	c.value = path;
 	filedialog.FileList(path);
 }
-function closefileDialog()
-{
+function closefileDialog() {
+	"use strict";
 	var i = document.getElementsByClassName("popup_center")[0];
-	i.style.display="none";
+	i.style.display = "none";
 }
 
-function open_selectedFile()
-{
-	console.log("OPENPATH:"+tarPath);
+function open_selectedFile() {
+	"use strict";
+	console.log("OPENPATH:" + tarPath);
 	closefileDialog();
 	registerProjectHistory(tarPath);
 	openProject(tarPath);
 }
 
-//-------------
-// project history
-
-function xrequet(url, callback_online, callback_offline)
-{
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "project_history.json", true);
-	xhr.onreadystatechange = function(){
-		if (xhr.readyState === 4 && xhr.status === 200){ // online
-			if (callback_online)
-				callback_online(this.responseText);
-		}
-		if (xhr.readyState === 4 && xhr.status === 0){ // offline
-			if (callback_offline)
-				callback_offline(this.responseText);
-		}
-	}
-	xhr.send(null);
-}
