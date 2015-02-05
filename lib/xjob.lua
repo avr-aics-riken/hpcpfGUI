803eceb808eddc0432bdbd4ba9a0bd6cb2f2c8ea
kioku <kentaro-o@digirea.com>
2015-02-05 19:53:21 +0900

    apply VINAS patch

--- lib/xjob.lua
+++ lib/xjob.lua
@@ -3,7 +3,19 @@
 
 function uploadFile(conf, localfile, remotefile)
     if (not remotefile) then remotefile = "./" end
-    local scpcmd = 'scp -i '.. conf.sshkey .. ' ' .. localfile .. ' ' .. conf.user ..'@'.. conf.server .. ':' .. remotefile;
+    --local scpcmd = 'scp -i '.. conf.sshkey .. ' ' .. localfile .. ' ' .. conf.user ..'@'.. conf.server .. ':' .. remotefile;
+    local scpcmd = 'scp ';
+    if conf.sshkey ~= nil then
+        scpcmd = scpcmd .. '-i '.. conf.sshkey .. ' ';
+    end
+    if conf.port ~= nil then
+        scpcmd = scpcmd .. '-P '.. conf.port .. ' ';
+    end
+    scpcmd = scpcmd .. localfile .. ' ';
+    if conf.user ~= nil then
+        scpcmd = scpcmd .. conf.user ..'@';
+    end
+    scpcmd = scpcmd .. conf.server .. ':' .. remotefile;
     --print('CMD>' .. scpcmd)
     local handle = io.popen(scpcmd)
     local result = handle:read("*a")
@@ -14,7 +26,18 @@ end
 
 function downloadFile(conf, remotefile, localfile)
     if (not localfile) then localfile = "./" end
-    local scpcmd = 'scp -i '.. conf.sshkey .. ' ' .. conf.user ..'@'.. conf.server .. ':' .. remotefile ..  ' ' .. localfile;
+    --local scpcmd = 'scp -i '.. conf.sshkey .. ' ' .. conf.user ..'@'.. conf.server .. ':' .. remotefile ..  ' ' .. localfile;
+    local scpcmd = 'scp ';
+    if conf.sshkey ~= nil then
+        scpcmd = scpcmd .. '-i '.. conf.sshkey .. ' ';
+    end
+    if conf.port ~= nil then
+        scpcmd = scpcmd .. '-P '.. conf.port .. ' ';
+    end
+    if conf.user ~= nil then
+        scpcmd = scpcmd .. conf.user ..'@';
+    end
+    scpcmd = scpcmd .. conf.server .. ':' .. remotefile .. ' ' .. localfile;
     --print('CMD>' .. scpcmd)
     local handle = io.popen(scpcmd)
     local result = handle:read("*a")
@@ -26,12 +49,23 @@ end
 
 --- Remote(ssh) commands
 
-local function sshCmd(user, server, key, cmd, disableErr)
+local function sshCmd(user, server, port, key, cmd, disableErr)
 	local nullDev = '/dev/null'
 	if (getPlatform() == 'Windows') then
 		disableErr = false
 	end
-    local sshcmd = 'ssh -i '.. key .. ' ' .. user ..'@'.. server .. ' "' .. cmd ..'"' .. (disableErr and (' 2>'..nullDev) or '')
+    --local sshcmd = 'ssh -i '.. key .. ' ' .. user ..'@'.. server .. ' "' .. cmd ..'"' .. (disableErr and (' 2>'..nullDev) or '')
+    local sshcmd = 'ssh ';
+    if key ~= nil then
+        sshcmd = sshcmd .. '-i '.. key .. ' ';
+    end
+    if port ~= nil then
+        sshcmd = sshcmd .. '-p '.. port .. ' ';
+    end
+    if user ~= nil then
+        sshcmd = sshcmd .. user ..'@';
+    end
+    sshcmd = sshcmd .. server .. ' "' .. cmd ..'"' .. (disableErr and (' 2>'..nullDev) or '')
 	--print('CMD>' .. sshcmd)
     local handle = io.popen(sshcmd)
     local result = handle:read("*a")
@@ -43,13 +77,13 @@ end
 function remoteExtractFile(conf, filepath, varbose)
     local option = verbose and 'xvf' or 'xf'
     local cmd = 'tar ' .. option .. ' ' .. filepath
-    return sshCmd(conf.user, conf.server, conf.sshkey, cmd)
+    return sshCmd(conf.user, conf.server, conf.port, conf.sshkey, cmd)
 end
 
 function remoteCompressFile(conf, srcfile, tarfile, verbose)
     local option = verbose and 'czvf' or 'czf'
     local cmd = 'tar ' .. option .. ' ' .. tarfile .. ' ' .. srcfile
-    return sshCmd(conf.user, conf.server, conf.sshkey, cmd)
+    return sshCmd(conf.user, conf.server, conf.port, conf.sshkey, cmd)
 end
 
 function remoteDeleteFile(conf, filename)
@@ -136,7 +170,7 @@ function remoteJobSubmit(conf, jobdata)
     local cmdTarget = 'cd ' .. jobdata.targetpath ..';'
     local cmdSubmit = cmdTarget ..  conf.submitCmd .. ' ' .. jobdata.job
 	--print(cmdSubmit)
-    local cmdret = sshCmd(conf.user, conf.server, conf.sshkey, cmdSubmit, true)
+    local cmdret = sshCmd(conf.user, conf.server, conf.port, conf.sshkey, cmdSubmit, true)
     local jobid = parseJobID(conf, cmdret)
     jobdata.id = jobid
     --print('JOB ID = '.. jobid)
@@ -150,13 +184,13 @@ function remoteJobDel(conf, jobdata)
 	end
     local cmdDel = conf.delCmd .. ' ' .. jobdata.id
 	--print(cmdDel)
-    local cmdret  = sshCmd(conf.user, conf.server, conf.sshkey, cmdDel, true)
+    local cmdret  = sshCmd(conf.user, conf.server, conf.port, conf.sshkey, cmdDel, true)
 end
 
 function remoteJobStat(conf, jobdata)
     local cmdStat = conf.statCmd .. ' ' .. (jobdata.id and jobdata.id or '')
 	--print(cmdStat)
-    local cmdret  = sshCmd(conf.user, conf.server, conf.sshkey, cmdStat, true)
+    local cmdret  = sshCmd(conf.user, conf.server, conf.port, conf.sshkey, cmdStat, true)
     local jobstat = parseJobStat(conf, cmdret, jobdata.id)
 	--print('JOB ST = ' .. jobstat)
     return jobstat
