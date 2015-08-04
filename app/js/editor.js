965f8e97948fa716f093be1a8ffaf9ff80476757
KazumaHatta <tori31001@gmail.com>
2015-08-04 17:43:04 +0900

    add project name to node editor

--- app/js/editor.js
+++ app/js/editor.js
@@ -251,6 +251,7 @@
 	function setProjectName(name) {
 		document.title = name;
 		$('info_project_title').innerHTML = "Project Name:";
+		$('node_title_text').innerHTML = name;
 		$('info_title_text').innerHTML = name;
 		$('exe_project_title_text').innerHTML = name;
 	}
