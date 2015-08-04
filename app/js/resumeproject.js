4521053a0153ca1d063640fa459c7b1af426c297
KazumaHatta <tori31001@gmail.com>
2015-08-04 15:25:11 +0900

    add resume list to resume project

--- app/js/resumeproject.js
+++ app/js/resumeproject.js
@@ -6,14 +6,51 @@
 	var socket = io.connect(),
 		resumeproject = {};
 	
+	
+	function clear() {
+		var resumeList = document.getElementById('resumeList');
+		resumeList.innerHTML = "";
+	}
+	
+	function addRow(name, status) {
+		var list = document.getElementById('resumeList'),
+			row = document.createElement('div');
+
+		row.innerHTML = "status:" + status + "   name:" + name;
+		list.appendChild(row);
+	}
+	
 	function init() {
 		var list = document.createElement('div'),
-			row = document.createElement('div');
-		
-		row.innerHTML = "hogehoge"
-		list.appendChild(row);
-		regiterlist.appendChild(list);
+			projectList = document.getElementById('projectlist');
+	
+		socket.emit('reqGetProjectList');
+		list.id = "resumeList";
+		list.className = "resumeList";
+		projectList.appendChild(list);
+		addRow("hogehoge", "test");
 	}
+	
+	socket.on('getProjectList', function (datastr) {
+		var data,
+			i,
+			name,
+			status;
+		
+		try {
+			clear();
+			data = JSON.parse(datastr);
+			for (i in data) {
+				if (data.hasOwnProperty(i)) {
+					name = i;
+					status = data[i];
+					addRow(name, status);
+				}
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	});
 	
 	window.resumeproject = resumeproject;
 	window.resumeproject.init = init;
